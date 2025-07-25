6c02ef745524a45cd19431468e4d35f208f682c5
YOGESHVENKATAPTHI <81062720+YOGESHVENKATAPTHI@users.noreply.github.com>
2025-07-25 23:53:59 +0530

    Add files via upload

--- client/src/pages/Option.tsx
+++ client/src/pages/Option.tsx
@@ -15,7 +15,7 @@ export function Option() {
         <AboutUsSection />
       </div>
       <AboutHostSection />
-      
+
       <WhySponsorSection />
       <div id="agenda">
         <EventAgendaSection />
